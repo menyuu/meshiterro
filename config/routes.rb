a2f3b3952cf29d88b1c1fa43a17f2a555f1b68ed
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-03 04:28:19 +0000

    [Finish] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :index, :show, :create]
   devise_for :users
   get "homes/about", as: "about"
   root to: "homes#top"
